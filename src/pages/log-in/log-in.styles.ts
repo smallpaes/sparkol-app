9ad3152443c5ba23cd91390dec28a1ef10a04a99
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-10 20:25:10 +0100

    Updated login component styling

--- src/pages/log-in/log-in.styles.ts
+++ src/pages/log-in/log-in.styles.ts
@@ -7,12 +7,24 @@ export const LogInContainer = styled.section`
 `;
 
 export const LogInForm = styled.form`
+  position: relative;
   max-width: 450px;
   margin: 100px auto 0 auto;
   background-color: ${({ theme }) => theme.colors.light[100]};
   border-radius: ${({ theme }) => theme.borderRadius.md};
   box-shadow: ${({ theme }) => theme.boxShadow.sm};
   padding: 1rem;
+  overflow: hidden;
+  &::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: '';
+    display: block;
+    height: 0.2rem;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.primary};
+  }
 `;
 
 export const LoginFormFieldSet = styled.fieldset`
@@ -26,4 +38,5 @@ export const LogInFormLegend = styled.legend`
   font-size: ${({ theme }) => theme.fontSizes.xxxl};
   text-align: center;
   color: ${({ theme }) => theme.colors.tertiary};
+  padding: 0.5rem;
 `;
