28a5f7988826a1c32cffcd1c13c890c7fbafb8a5
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-11 23:28:28 +0100

    Refactored setDataToLocalStorage type

--- src/pages/log-in/helpers/setDataToLocalStorage.ts
+++ src/pages/log-in/helpers/setDataToLocalStorage.ts
@@ -1,4 +1,6 @@
-const setDataToLocalStorage = <T>(key: string, data: T) => {
+import { SetDataToLocalStorage } from '../log-in.types';
+
+const setDataToLocalStorage: SetDataToLocalStorage = (key, data) => {
   if (!key || !data) throw new Error('Key or data is empty');
   localStorage.setItem(key, JSON.stringify(data));
 };
