1e6131cfebd3939f3d8393fedfa9686eb375b5da
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-11 17:04:28 +0100

    Refactored form input logic

--- src/pages/log-in/log-in.component.tsx
+++ src/pages/log-in/log-in.component.tsx
@@ -15,19 +15,65 @@ import useIsValidPassword from './hooks/useIsValidPassword';
 import useIsValidUserName from './hooks/useIsValidUserName';
 import useLogin from './hooks/useLogin';
 
-const LogIn: FC = () => {
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [isUserNameTouched, setIsUserNameTouched] = useState(false);
-  const [isPasswordTouched, setIsPasswordTouched] = useState(false);
+interface IField {
+  value: string;
+  isTouched: boolean;
+}
 
-  const isUserNameValid = useIsValidPassword(userName);
-  const isPasswordValid = useIsValidUserName(password);
-  const { login, error, isLoading } = useLogin(userName, password);
+interface IFormData {
+  userName: IField;
+  password: IField;
+}
+
+const LogIn: FC = () => {
+  const [formData, setFormData] = useState<IFormData>({
+    userName: {
+      value: '',
+      isTouched: false,
+    },
+    password: {
+      value: '',
+      isTouched: false,
+    },
+  });
+
+  const updateFormData = (fieldName: keyof IFormData, value: IField) => {
+    if (fieldName === undefined || value === undefined) return;
+    if (formData[fieldName] === undefined) return;
+    setFormData({
+      ...formData,
+      [fieldName]: value,
+    });
+  };
+
+  const isUserNameValid = useIsValidPassword(formData.userName.value);
+  const isPasswordValid = useIsValidUserName(formData.password.value);
+  const { login, error, isLoading } = useLogin(
+    formData.userName.value,
+    formData.password.value,
+  );
 
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     login();
+  };
+
+  const handleInputValueChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    fieldName: keyof IFormData,
+  ) => {
+    const { value } = e.target;
+    updateFormData(fieldName, {
+      ...formData[fieldName],
+      value,
+    });
+  };
+
+  const handleInputTouched = (fieldName: keyof IFormData) => {
+    updateFormData(fieldName, {
+      ...formData[fieldName],
+      isTouched: true,
+    });
   };
 
   return (
@@ -38,30 +84,28 @@ const LogIn: FC = () => {
           {error && <LogInFormWarningMessage>{error}</LogInFormWarningMessage>}
           <LogInInputGroup>
             <FormInput
-              isTouched={isUserNameTouched}
+              isTouched={formData.userName.isTouched}
               isInvalid={!isUserNameValid}
               invalidMessage="Please enter your user name"
               type="text"
               name="User Name"
               id="userName"
-              value={userName}
+              value={formData.userName.value}
               aria-invalid={!isUserNameValid}
-              onChange={(e) => setUserName(e.target.value)}
-              onBlur={() => setIsUserNameTouched(true)}
-              placeholder="User name"
+              onChange={(e) => handleInputValueChange(e, 'userName')}
+              onBlur={() => handleInputTouched('userName')}
             />
             <FormInput
-              isTouched={isPasswordTouched}
+              isTouched={formData.password.isTouched}
               isInvalid={!isPasswordValid}
               invalidMessage="Please enter your password"
               type="password"
               name="Password"
               id="password"
-              value={password}
+              value={formData.password.value}
               aria-invalid={!isPasswordValid}
-              onChange={(e) => setPassword(e.target.value)}
-              onBlur={() => setIsPasswordTouched(true)}
-              placeholder="Password"
+              onChange={(e) => handleInputValueChange(e, 'password')}
+              onBlur={() => handleInputTouched('password')}
               autoComplete="password"
             />
           </LogInInputGroup>
