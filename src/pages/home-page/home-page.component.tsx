36c96153ebae3ca619968512bb5e1e617d116759
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-11 16:19:46 +0100

    Added logout functionality and added to home page, and updatted home page layout

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -1,7 +1,34 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
+
+import {
+  HomePageContainer,
+  HomePageContent,
+  HomePageTitle,
+  HomePageSubtitle,
+  HomePageButton,
+} from './home-page.styles';
+import { UserContext } from '../../context/UserContext';
+import useLogOut from '../../hooks/useLogOut';
+import { ButtonType } from '../../components/button/button.types';
 
 const HomePage: FC = () => {
-  return <div>HomePage</div>;
+  const { user } = useContext(UserContext);
+  const logOut = useLogOut();
+  return (
+    <HomePageContainer>
+      <HomePageContent>
+        <HomePageTitle>Welcome, {user?.name}</HomePageTitle>
+        <HomePageSubtitle>You are logged in</HomePageSubtitle>
+      </HomePageContent>
+      <HomePageButton
+        buttonType={ButtonType.PRIMARY}
+        type="button"
+        onClick={logOut}
+      >
+        Log Out
+      </HomePageButton>
+    </HomePageContainer>
+  );
 };
 
 export default HomePage;
