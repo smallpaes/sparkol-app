20b924b568fe1d8e3b5d7cc3d3c7b9a3154affba
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-12 10:14:27 +0100

    Added test cases for useLogOut hook

---
+++ src/hooks/__tests__/useLogOut.test.tsx
@@ -0,0 +1,38 @@
+import { expect, test, describe, vi } from 'vitest';
+import { renderHook } from '@testing-library/react';
+
+import useLogOut from '../useLogOut';
+import { UserContext } from '../../context/UserContext';
+import { IDefaultUser, User } from '../../types/user';
+
+describe('useLogOut', () => {
+  test('should return a logOut function', () => {
+    const mockUser: IDefaultUser = {
+      user: null,
+      clearUser: () => undefined,
+      setUser: () => undefined,
+    };
+    const { result } = renderHook(() => useLogOut(), {
+      wrapper: ({ children }) => (
+        <UserContext.Provider value={mockUser}>{children}</UserContext.Provider>
+      ),
+    });
+    expect(result.current).toBeInstanceOf(Function);
+  });
+
+  test('should call clearUser when the return function is called', async () => {
+    const mockClearUser = vi.fn();
+    const mockUser: IDefaultUser = {
+      user: null,
+      clearUser: mockClearUser,
+      setUser: () => undefined,
+    };
+    const { result } = renderHook(() => useLogOut(), {
+      wrapper: ({ children }) => (
+        <UserContext.Provider value={mockUser}>{children}</UserContext.Provider>
+      ),
+    });
+    await result.current();
+    expect(mockClearUser).toHaveBeenCalled();
+  });
+});
