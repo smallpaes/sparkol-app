c12784dc98b374077699d6809dbea5ad3d81b319
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-12 23:03:46 +0100

    Parsed token retrieved from localStorage

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -12,7 +12,7 @@ axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${JSON.parse(token)}`;
     }
     return config;
   },
