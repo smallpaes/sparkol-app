5cf9aa311b07ed037cf19ba6a227dea7028d5e07
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-13 10:43:05 +0100

    Added a test case for HomePage button

--- src/pages/home-page/__tests__/home-page.test.tsx
+++ src/pages/home-page/__tests__/home-page.test.tsx
@@ -1,4 +1,4 @@
-import { expect, test, describe } from 'vitest';
+import { expect, test, describe, vi } from 'vitest';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 
@@ -6,6 +6,7 @@ import HomePage from '../home-page.component';
 import { UserContext } from '../../../context/UserContext';
 import { IDefaultUser } from '../../../types/user';
 import { theme } from '../../../theme';
+import * as useLogOut from '../../../hooks/useLogOut';
 
 describe('HomePage', () => {
   test('should render the home page skeleton', async () => {
@@ -98,5 +99,33 @@ describe('HomePage', () => {
     const button = await homePage.findByTestId('button');
     expect(button.textContent).toContain('Log Out');
     homePage.unmount();
+  });
+
+  test('should call the log out function when the log out button is clicked', async () => {
+    const mockLogOut = vi.fn();
+    const spyUseLogOut = vi
+      .spyOn(useLogOut, 'default')
+      .mockReturnValue(mockLogOut);
+    const mockUser: IDefaultUser = {
+      user: {
+        name: 'Test User',
+        id: 1,
+        token: 'test-token',
+      },
+      clearUser: () => undefined,
+      setUser: () => undefined,
+    };
+    const homePage = render(
+      <ThemeProvider theme={theme}>
+        <UserContext.Provider value={mockUser}>
+          <HomePage />
+        </UserContext.Provider>
+      </ThemeProvider>,
+    );
+
+    const button = await homePage.findByTestId('button');
+    button.click();
+    expect(spyUseLogOut).toHaveBeenCalled();
+    expect(mockLogOut).toHaveBeenCalled();
   });
 });
